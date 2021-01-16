09bdf4a67d011f4d190325bcb2edcd448778fe06
chillianproject <76863353+chillianproject@users.noreply.github.com>
2021-01-16 15:18:46 +0000

    Deploying to host from  @ d93ad618ce27bb2f8caab760aecfd472d9a71daa 🚀

--- static/js/app.3a9a0473.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{52:function(t,e,n){t.exports=n(65)},65:function(t,e,n){"use strict";n.r(e);var a=n(68),r=n(0),o=n.n(r),c=n(3),s=n(46),p=n(11);var i=c.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}});Object(a.a)((function(){return o.a.createElement(p.a,{style:i.container},o.a.createElement(s.a,null,"Open up App.tsx to start working on your app!"))}))}},[[52,1,2]]]);
-//# sourceMappingURL=app.3a9a0473.chunk.js.map
